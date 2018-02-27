e171e4ee4bc1028a921e3b1ff7ad4a05e529ad92
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2018-02-27 11:18:00 -0400

    ✨ google maps

--- camera-app/src/pages/home/home.ts
+++ camera-app/src/pages/home/home.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  image: string = null;
 
+  constructor(
+    public navCtrl: NavController,
+    private camera: Camera
+  ) {
+
+  }
+
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      allowEdit: true,
+      correctOrientation: true,
+      sourceType: this.camera.PictureSourceType.CAMERA
+    };
+    this.camera.getPicture(options)
+    .then(image => {
+      console.log('image', image);
+      this.image = image;
+    })
+    .catch(error => console.log(error));
+  }
+
+  goToMapPage() {
+    this.navCtrl.push('MapPage');
   }
 
 }
